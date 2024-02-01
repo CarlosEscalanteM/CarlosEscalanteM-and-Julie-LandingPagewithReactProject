8e8b4f5cfd4099e532cdc54f0568355d78501de9
Yeju Lee (Julie) Motley <149555844+yjlmotley@users.noreply.github.com>
2024-02-01 22:57:23 +0000

    Updated Footer

--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -3,11 +3,12 @@
 import React from "react";
 
 const Footer = () => {
+    const year = new Date().getFullYear();
     return (
         <footer className="footer bg-black text-white">
             <div className="container-fluid pt-4 pb-3 text-center">
                 <p className="fs-6">
-                    Copyright <i className="fa-regular fa-copyright" style={{ color: '#ffffff' }}></i> Your Website 2018
+                    {`Copyright © Your Website ${year}`}
                 </p>
             </div>
         </footer>
